24cd41481aaa0b5544543619691b7287335d9043
chuckunix <Dmitry.Pimenov@gmail.com>
2025-05-19 13:57:45 +0000

    first config

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "vcn01" {
+	source = "./vcn_module"
+	cidr_block	= var.vcn01_cidr_block
+	dns_label      = var.vcn01_display_name
+	compartment_id	= var.compartment_ocid
+	display_name = var.vcn01_display_name
+}
+
+module "vcn02" {
+	source = "./vcn_module"
+	display_name = "vcn02"
+	cidr_block	= "172.0.0.0/24"
+	compartment_id	= var.compartment_ocid
+	dns_label = "vcn02"
+}
+
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+	description = "region where you have tenancy"
+	type	= string
+	default = "eu-frankfurt-1"
+}
+variable "compartment_ocid" {}
+variable "vcn01_cidr_block" {}
+variable "vcn01_display_name" {}
